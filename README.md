d6133a2a6e52485ef098424e7bf98a09ef3342bd
Seth <sfhiono@gmail.com>
2018-02-15 21:21:55 +0800

    Seth
    cleaning the repo

--- README.md
+++
@@ -1,5 +0,0 @@
-# Pet Lovers Community
-
-A pet adoption website 
-
-A BSCS 3A and 3B collaboration for the courses Software Engineering and Web Programming.
--- application/views/template/header.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>HelloApp | <?php echo $title; ?></title>
-	<link rel="stylesheet" href="<?php echo base_url('assets/css/default.css') ?>"/>
-</head>
-<body>
