7f45a16e4330542a18065c969f84360356529991
Silvestre511 <rhed_silvestre@yahoo.com.ph>
2018-03-20 11:14:55 +0800

    Posting.v.1.3 (w/ Community/Recent posts working)

--- application/controllers/plcrecent.php
+++ application/controllers/plcrecent.php
@@ -3,13 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class plcrecent extends CI_Controller {
   public function posts(){
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
       $this->load->model('recentposts_model');
       $data['pst']=$this->recentposts_model->posts();
       $this->load->view('template/header');
       $this->load->view('pages/recent', $data);
       $this->load->view('template/footer');
-    }
   }
 }
 ?>
